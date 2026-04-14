5092a339b3a30fa960a35f4dc3a5fea2d26066fc
Gabe Thomas <gabethomas@mac.mynetworksettings.com>
2026-04-13 21:38:52 -0400

    Smooth in-app navigation and gradient overscroll

--- ios/App/App/AppViewController.swift
+++ ios/App/App/AppViewController.swift
@@ -4,18 +4,35 @@ import Capacitor
 
 class AppViewController: CAPBridgeViewController {
     private let shellBackground = UIColor(red: 238.0 / 255.0, green: 244.0 / 255.0, blue: 255.0 / 255.0, alpha: 1)
+    private let topGradient = UIColor(red: 247.0 / 255.0, green: 250.0 / 255.0, blue: 255.0 / 255.0, alpha: 1).cgColor
+    private let bottomGradient = UIColor(red: 255.0 / 255.0, green: 247.0 / 255.0, blue: 239.0 / 255.0, alpha: 1).cgColor
+    private let gradientLayer = CAGradientLayer()
 
     override open func capacitorDidLoad() {
         super.capacitorDidLoad()
 
         view.backgroundColor = shellBackground
+        gradientLayer.colors = [topGradient, shellBackground.cgColor, bottomGradient]
+        gradientLayer.locations = [0.0, 0.58, 1.0]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        if gradientLayer.superlayer == nil {
+            view.layer.insertSublayer(gradientLayer, at: 0)
+        }
 
         guard let webView = webView else { return }
         webView.isOpaque = false
-        webView.backgroundColor = shellBackground
-        webView.scrollView.backgroundColor = shellBackground
+        webView.backgroundColor = .clear
+        webView.scrollView.backgroundColor = .clear
+        webView.scrollView.bounces = true
+        webView.scrollView.alwaysBounceVertical = true
         webView.scrollView.showsVerticalScrollIndicator = false
         webView.scrollView.showsHorizontalScrollIndicator = false
         webView.scrollView.keyboardDismissMode = .interactive
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = view.bounds
     }
 }
